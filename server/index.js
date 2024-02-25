23795d29730890d31f3b6ca034938e9a8ee29b74
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2024-02-26 02:05:37 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const upload = multer({
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["*"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
